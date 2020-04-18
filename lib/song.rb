34d1ebe67ee9bc76e0e0fdbeda9726689a4b9551
John Zonneveld <jjczon@gmail.com>
2020-04-18 20:27:26 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -10,14 +10,6 @@ class Song
 
   @@songs = []
 
-  def initialize
-    @@songs << self
-  end
-
-  # def self.find_by_name(name)
-  #   @@songs.detect{|a| a.name == name}
-  # end
-
   def self.all
     @@songs
   end
@@ -26,7 +18,4 @@ class Song
     @artist = artist
   end
 
-  def to_param
-    name.downcase.gsub(' ', '-')
-  end
 end
